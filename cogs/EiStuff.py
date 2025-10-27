6470d10c23bd26a57a26ff2887ca1168b6e002ed
gms-e <becomeanimenow@gmail.com>
2025-10-27 12:14:42 -0700

    made testpingai take string literal, removed mariofan

--- cogs/EiStuff.py
+++ cogs/EiStuff.py
@@ -1,7 +1,7 @@
 import requests
 import discord
 from discord.ext import commands
-from typing import Optional
+from typing import Optional, Literal
 import random
 import asyncio
 import string
@@ -19,7 +19,7 @@ class EiStuff(commands.Cog):
     async def on_ready(self):
         print("Ei Stuff online")
     @commands.hybrid_command(name = "testpingai")
-    async def ei(self, ctx, guy: discord.Member, inputted: str):
+    async def ei(self, ctx, guy: Literal["Astro", "CB"], inputted: str):
         API_KEY_1 = os.getenv("API_KEY_1")
         try:
 
@@ -36,7 +36,7 @@ class EiStuff(commands.Cog):
                     "messages": [
                         {
                             "role": "user",
-                            "content": f"based on the following messages from {guy.global_name}, generate only one sentence message seed {random.random()}as them with no additional commentary"
+                            "content": f"based on the following messages from {guy}, generate only one sentence message seed {random.random()}as them with no additional commentary"
                         },
                         {
                             "role": "assistant",
@@ -61,27 +61,27 @@ class EiStuff(commands.Cog):
             print(response.json())
             print(response.json()["choices"][0]["message"]["content"])
             if response.json()["choices"][0]["message"]["reasoning"] is not None and response.json()["choices"][0]["message"]["content"] is not None and 0 < len(response.json()["choices"][0]["message"]["reasoning"]) < len(response.json()["choices"][0]["message"]["content"]):
-                await initial_message.edit(content= f"{guy.display_name}" + ":\n" + response.json()["choices"][0]["message"]["reasoning"])
+                await initial_message.edit(content= f"{ctx.author}: \n{inputted}\n{guy}" + ":\n" + response.json()["choices"][0]["message"]["reasoning"])
             else:
-                await initial_message.edit(content = f"{guy.display_name}" + ":\n" + response.json()["choices"][0]["message"]["content"])
+                await initial_message.edit(content = f"{guy}" + ":\n" + response.json()["choices"][0]["message"]["content"])
         except Exception as e:
             print(response.json())
-            await initial_message.edit(content=f"{type(e)}")
+            await initial_message.edit(content=f"{type(e)}\n{str(e)}")
             print(e)
             print(type(e))
 
-    async def pullQaM(self, message, guy: discord.Member):
+    async def pullQaM(self, message, guy: str):
         iconic = ""
         try:
-            match guy.id:
-                case 450811106504605706:#anth
+            match guy:
+                case "Anth":#anth
                     pulled = 0
-                case 770464351336923157:#astro
+                case "Astro":#astro
                     iconic = "See the image means peach likes mario but mario wants to moon peach\nI can't do math, this is why I need to take drugs more\nI absolutely just DEMOLISHED that child\nThey're all being so god damn HORNY on me\nstop being pessamadistic... be sadistic instead\nJoshua deez nuts\nI fucking hate the stupid black guy\nBy the way, I got better inflation. Now my balls will be even bigger\nDammit! Short people! My main enemy\nwhat if you do double single and a half backflip\nHe has stage three schizophrenia, it's terminal. He's gonna become Edwosk soon\nMy brother could do better than you and I'm an only child\nCouldn't be in Elevenessee? Lmao\nNotice how it's sab-cuh-duh-fuh, not sab-cuh-def?\nIt's like if you gave a toddler steroids\nwell some people are from canada or at least they claim to be, so they use canadian dollars\nOmar I'm gonna give you relationship issues\nDon't bomb me I'm not Japan\nRoses are red, Omar is dumb, I am going to beat you to death with my thumb\nah, nOoOo, gRaNdPa!\nYou have rights but what'll happen to you is very wrong.\nIt's the cycle of life, Mario, you wouldn't understand. You don't have a life\nI would have loved you if you didn't suck so much\nI love hearing the sound of Edwosk's suffering and impending doom\nI'm going to touch you, I'm going to touch you\nIf you want to touch your balls, then you want to know if you'll get set on fire first\nAlright I got it, the double whammy kablammy\nCome on Mario, 3 towers is more towers than the U.S. has lost\nI probably should have told you this earlier, but I started a cult in math class and we worship Joshua\nYou said no to my deez nuts, so I say no to DOZE nuts!\nBoingy toingy give me your... groiny\nIf I'm awake then I'll bone with you\nYou're smelting my balls?\nI'd say the n word but this is useful info\nWhy die in a fire when I can swim in a vat of acid?\nbedocome quikerlyness\nGive me some.. people\nMario your gonna go up there and your gonna like it"
                     async for m in message.channel.history(limit = 170):
                         if m.author == guy:
                             iconic = iconic + "\n" + m.content
-                case 456858402832908301:#cb
+                case "CB":#cb
                     iconic = "Ok, time to go jerk off!\nHow are you doing, small child? Get back into the field where you belong\nim the first one to beat goku with unpaid child support bills\nIf anyone asks.. Josh died in a drunk driving accident\nI need you to take a hit while I run away, don't worry if you start convulsing on the floor because you're paralyzed\nHe's gotta tighten some nuts if you know what I mean\nCan't believe I'm doing it with you\nmods... cut off his balls\nthe n word\nbut you are white, so\nQuickly! I gotta look up Among Us roleplay!\nThe gay rights, the gay middles, and the gay lefts will not stop me\nSometimes people just think you're a nazi\nI'm a woman driver so it makes sense\nGay\noh yeah, it does have a hole!\nGot it, slave labour!\nDonkey Kong was riding my ass. You heard me, he got real close\nDang you can screw Luigi?\ndon't call me racist but why is it black now?\nGoogle, show me this man's balls\nI'm about to tear up the entire ecosystem that's next to me because it's blocking my view\nYou ever seen a more depressed giraffe?\nWe are NOT turning this into a foot fetish\nYou know what? Screw it, forbidden milk time.\nyou watched them fuck, I know you did!\nlet's go guys we can cream through this block\nIt's not short for anything, he's not black\ndis is my schlong.\nI'm trying to find the one. I just have to look through all the rule 34 to get to it.\nThat's just furry stuff waiting to happen"
                     async for m in message.channel.history(limit=370):
                         if m.author == guy and len(iconic) < 1000:
@@ -92,12 +92,8 @@ class EiStuff(commands.Cog):
                     pulled = 3
                 case 721389007426158633:#josh
                     pulled = 4
-                case 405197452833062912:#mariofan
-                    iconic = "Guess he's eating Hitler ¯\\_(ツ)_/¯\nI saw a guy do this in a toothpaste ad once!\nYou know what I do make? Crypto scams\nfire in the hole!\nJosh is like cancer\nCan you beat... your wife.\nI watched you and you just killed yourself\nOh I've got an idea! we recreate 9/11\nastro that joke is dead. Just like Mario and Luigi s father.\nthe tables are encircling the dressers from, behind"
-                    async for m in message.channel.history(limit = 170):
-                        if m.author == guy:
-                            iconic = iconic + "\n" + m.content
-                            # print(iconic)
+                #case :#mf
+                    #no.
                 case 925472450962141195:#meawor
                     pulled = 6
                 case 702906770003198003:#om
