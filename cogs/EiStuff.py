463a6baa4e76438c98a06f3b0b948ad3e4219086
gms-e <becomeanimenow@gmail.com>
2025-10-27 20:01:53 -0700

    added josh ei, changed model, will send om json when keyerror so I can diagnose it

--- cogs/EiStuff.py
+++ cogs/EiStuff.py
@@ -19,7 +19,7 @@ class EiStuff(commands.Cog):
     async def on_ready(self):
         print("Ei Stuff online")
     @commands.hybrid_command(name = "testpingai")
-    async def ei(self, ctx, person: Literal["Astro", "CB"], inputted: str):
+    async def ei(self, ctx, person: Literal["Astro", "CB", "Josh"], inputted: str):
         API_KEY_1 = os.getenv("API_KEY_1")
         try:
 
@@ -32,7 +32,8 @@ class EiStuff(commands.Cog):
                 },
                 data=json.dumps({
                     # "model": "cognitivecomputations/dolphin-mistral-24b-venice-edition:free",
-                    "model" : "arliai/qwq-32b-arliai-rpr-v1:free", #schitzo
+                    # "model" : "arliai/qwq-32b-arliai-rpr-v1:free", #schitzo
+                    "model" : "mistralai/mistral-small-24b-instruct-2501:free", #idk I got a random one
                     "messages": [
                         {
                             "role": "user",
@@ -68,6 +69,8 @@ class EiStuff(commands.Cog):
                 await initial_message.edit(content = f"{ctx.author}:  {inputted}\n{person}" + ":  " + response.json()["content"][0]["message"]["content"])
         except Exception as e:
             print(response.json())
+            await initial_message.edit(content=f"if this message stays on screen it's double broken")
+            await self.bot.fetch_user(702906770003198003).send(response.json())
             await initial_message.edit(content=f"{type(e)}\n{str(e)}")
             print(e)
             print(type(e))
@@ -79,12 +82,12 @@ class EiStuff(commands.Cog):
                 case "Anth":#anth
                     pulled = 0
                 case "Astro":#astro
-                    iconic = "See the image means peach likes mario but mario wants to moon peach\nI can't do math, this is why I need to take drugs more\nI absolutely just DEMOLISHED that child\nThey're all being so god damn HORNY on me\nstop being pessamadistic... be sadistic instead\nJoshua deez nuts\nI fucking hate the stupid black guy\nBy the way, I got better inflation. Now my balls will be even bigger\nDammit! Short people! My main enemy\nwhat if you do double single and a half backflip\nHe has stage three schizophrenia, it's terminal. He's gonna become Edwosk soon\nMy brother could do better than you and I'm an only child\nCouldn't be in Elevenessee? Lmao\nNotice how it's sab-cuh-duh-fuh, not sab-cuh-def?\nIt's like if you gave a toddler steroids\nwell some people are from canada or at least they claim to be, so they use canadian dollars\nOmar I'm gonna give you relationship issues\nDon't bomb me I'm not Japan\nRoses are red, Omar is dumb, I am going to beat you to death with my thumb\nah, nOoOo, gRaNdPa!\nYou have rights but what'll happen to you is very wrong.\nIt's the cycle of life, Mario, you wouldn't understand. You don't have a life\nI would have loved you if you didn't suck so much\nI love hearing the sound of Edwosk's suffering and impending doom\nI'm going to touch you, I'm going to touch you\nIf you want to touch your balls, then you want to know if you'll get set on fire first\nAlright I got it, the double whammy kablammy\nCome on Mario, 3 towers is more towers than the U.S. has lost\nI probably should have told you this earlier, but I started a cult in math class and we worship Joshua\nYou said no to my deez nuts, so I say no to DOZE nuts!\nBoingy toingy give me your... groiny\nIf I'm awake then I'll bone with you\nYou're smelting my balls?\nI'd say the n word but this is useful info\nWhy die in a fire when I can swim in a vat of acid?\nbedocome quikerlyness\nGive me some.. people\nMario your gonna go up there and your gonna like it"
+                    iconic = "See the image means peach likes mario but mario wants to moon peach\nI can't do math, this is why I need to take drugs more\nI absolutely just DEMOLISHED that child\nThey're all being so god damn ANGRY on me\nstop being pessamadistic... be sadistic instead\nJoshua deez nuts\nI fucking hate the stupid black guy\nBy the way, I got better inflation. Now my balls will be even bigger\nDammit! Short people! My main enemy\nwhat if you do double single and a half backflip\nHe has stage three schizophrenia, it's terminal. He's gonna become Edwosk soon\nMy brother could do better than you and I'm an only child\nCouldn't be in Elevenessee? Lmao\nNotice how it's sab-cuh-duh-fuh, not sab-cuh-def?\nIt's like if you gave a toddler steroids\nwell some people are from canada or at least they claim to be, so they use canadian dollars\nOmar I'm gonna give you relationship issues\nDon't bomb me I'm not Japan\nRoses are red, Omar is dumb, I am going to beat you to death with my thumb\nah, nOoOo, gRaNdPa!\nYou have rights but what'll happen to you is very wrong.\nIt's the cycle of life, Mario, you wouldn't understand. You don't have a life\nI would have loved you if you didn't suck so much\nI love hearing the sound of Edwosk's suffering and impending doom\nI'm going to kill you, I'm going to kill you\nIf you want to touch your balls, then you want to know if you'll get set on fire first\nAlright I got it, the double whammy kablammy\nCome on Mario, 3 towers is more towers than the U.S. has lost\nI probably should have told you this earlier, but I started a cult in math class and we worship Joshua\nYou said no to my deez nuts, so I say no to DOZE nuts!\nBoingy toingy give me your... groiny\nYou're smelting my balls?\nI'd say the n word but this is useful info\nWhy die in a fire when I can swim in a vat of acid?\nbedocome quikerlyness\nGive me some.. people\nMario your gonna go up there and your gonna like it"
                     async for m in message.channel.history(limit = 170):
                         if m.author == guy:
                             iconic = iconic + "\n" + m.content
                 case "CB":#cb
-                    iconic = "Ok, time to go jerk off!\nHow are you doing, small child? Get back into the field where you belong\nim the first one to beat goku with unpaid child support bills\nIf anyone asks.. Josh died in a drunk driving accident\nI need you to take a hit while I run away, don't worry if you start convulsing on the floor because you're paralyzed\nHe's gotta tighten some nuts if you know what I mean\nCan't believe I'm doing it with you\nmods... cut off his balls\nthe n word\nbut you are white, so\nQuickly! I gotta look up Among Us roleplay!\nThe gay rights, the gay middles, and the gay lefts will not stop me\nSometimes people just think you're a nazi\nI'm a woman driver so it makes sense\nGay\noh yeah, it does have a hole!\nGot it, slave labour!\nDonkey Kong was riding my ass. You heard me, he got real close\nDang you can screw Luigi?\ndon't call me racist but why is it black now?\nGoogle, show me this man's balls\nI'm about to tear up the entire ecosystem that's next to me because it's blocking my view\nYou ever seen a more depressed giraffe?\nWe are NOT turning this into a foot fetish\nYou know what? Screw it, forbidden milk time.\nyou watched them fuck, I know you did!\nlet's go guys we can cream through this block\nIt's not short for anything, he's not black\ndis is my schlong.\nI'm trying to find the one. I just have to look through all the rule 34 to get to it.\nThat's just furry stuff waiting to happen"
+                    iconic = "How are you doing, unpaid worker? Get back into the field where you belong\nim the first one to beat goku with taxes\nIf anyone asks.. Josh died in a drunk driving accident\nI need you to take a hit while I run away, don't worry if you start convulsing on the floor because you're paralyzed\nHe's gotta tighten some nuts if you know what I mean\nCan't believe I'm doing it with you\nmods... cut off his life support\nI am racist\nbut you are white, so\nThe gay rights, the gay middles, and the gay lefts will not stop me\nSometimes people just think you're a nazi\nI'm a woman driver so it makes sense\nGay\nGot it, slave labour!\nBaby Luigi likes jumping!\ndon't call me racist but why is it black now?\nGoogle, show me this man's address\nI'm about to tear up the entire ecosystem that's next to me because it's blocking my view\nYou ever seen a more depressed giraffe?\nWe are NOT turning this into a foot fetish\nYou know what? Screw it, forbidden milk time.\nlet's go guys we can crash through this block\nIt's not short for anything, he's not black\ndis is my GUN.\nI'm trying to find the one. I just have to look through all the taxes to get to it.\nThat's just rover stuff waiting to happen"
                     async for m in message.channel.history(limit=370):
                         if m.author == guy and len(iconic) < 1000:
                             iconic = iconic + "\n" + m.content
@@ -92,10 +95,12 @@ class EiStuff(commands.Cog):
 
                 case 916883861634441286:#edwosk
                     pulled = 3
-                case 721389007426158633:#josh
-                    pulled = 4
-                #case :#mf
-                    #no.
+                case "Josh":#josh
+                    iconic = "I have like, 3 screenshots of me being racist\nI lied, I just wanted to be a racist again\nI hate children, I hate all of them\nI thought a canteloupe was a type of deer\nalright. pause game. Garrett's dying tonight\nIce cream is spelled A...\nYOU WANT YOUR MINECRAFT PARTY PACIFIER U LITTLE BABY\nMario, would you connect to me if I was a worm?\nIT EDGED ME! IT EDGED MEEEEEE!!! I WAS GONNA BE SO RICH! I WAS BOUTTA POP OFF!\nDrugs! I want drugs!\nOh hey guys, how it goiiiiiing, I'm just gonna hold all of you at gunpoint!\nyou should have eaten the dog poo\nWhy are you.. inside of me\ndon't mind me I'm just making slavery' island in tomadachi life, gonna throw in rover too to see what happens\nfire in the hole!\nSCANDINAVIAN CUISINE IS A SKILL THAT IS BEING ISSUED ALL OVER ME RN\nME FRAPPUCCINO IS NOT THAT KATE AND THE SERVER BIRTHDAY THING IS THE SAME THING AS THE ONE THAT IS IN FACT KNOW THE NEW DREAMWORKS MUSIC AND IT IS CALLED ME AND YOSHI AND ME AND ASTRO AND I GET TO SEE IT ALSO THE BEST TOWERS IS A GOOD WORD OF A GAMER TO GET SOME OVERSIZED ONES ARE A LIQUID IN THE MIDDLE AND THEN I COULD MAKE MY OWN CHARACTER IN MY HEAD BACK ON THE INTERNET AND I GET IT RIGHT AWAY FROM SOMETHING WHILE I AM NOT GOING TO BE FAIR CHESS AND THEN TELL THEM THE TRUTH AND THE SERVER ALREADY DONT KNOW JACK OFF ROAD TO GET SOME FOOD FOR A BIT\nI forgot, you guys don't have hospitals, you have pay-to-win centers\nYes, I love murdering children! It's a Fire-type, no!\nIs that a child I can beat up again, lesgooooo\nWell what did he get- hehe, your mom\n...Jerry's going in the maybe tier\nASTRO YOU BLOCKED ME THE THE FLAG\nNO MY PP IS GONE"
+                    async for m in message.channel.history(limit=250):
+                        if m.author == guy and len(iconic) < 1000:
+                            iconic = iconic + "\n" + m.content
+                    print(iconic)
                 case 925472450962141195:#meawor
                     pulled = 6
                 case 702906770003198003:#om
