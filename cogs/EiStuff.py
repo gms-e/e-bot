6d990bfea3000566241867f35c7ba708c8686c5b
gms-e <becomeanimenow@gmail.com>
2025-10-29 12:10:06 -0700

    edwosk.

--- cogs/EiStuff.py
+++ cogs/EiStuff.py
@@ -77,7 +77,7 @@ class EiStuff(commands.Cog):
                         },
                         {
                             "role": "user",
-                            "content": f"Here are messages: {await self.pullQaM(ctx, person)}, rover is a catboy"
+                            "content": f"Here are messages: {await self.pullQaM(ctx, person)}, rover is a catboy, tier lists have an edwosk tier"
                         },
                         {
                             "role": "assistant",
