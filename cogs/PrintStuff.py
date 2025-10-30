a41d271192c9e26e4220f098307b32bf700d7395
gms-e <becomeanimenow@gmail.com>
2025-10-30 00:48:12 -0700

    fixed comp error

--- cogs/PrintStuff.py
+++ cogs/PrintStuff.py
@@ -252,7 +252,7 @@ class PrintStuff(commands.Cog):
         if "dev" in ctx.author.nick:
             with open("updayt.txt", 'r+') as f:
                 prev = f.readline()
-                f.write(str(days))
+                f.write(str(days) + "     ")
                 await ctx.send(f"changed {prev} days to {days}")
     @animation.group(name = "due", brief = "in")
     async def due(self, ctx):
