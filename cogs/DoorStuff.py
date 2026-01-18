631e122a51e314a6a501d23614ca0b529d363aec
gms-e <becomeanimenow@gmail.com>
2026-01-18 00:53:53 -0800

    added hour tracking to gimmick doors of doom channel

--- cogs/DoorStuff.py
+++ cogs/DoorStuff.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from typing import Optional
 import random
 from discord.ui import Button, View
@@ -10,6 +10,7 @@ import datetime
 class DoorStuff(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.timepass.start()
 
     async def printto(self, m: str):
         print(m)
@@ -83,6 +84,56 @@ class DoorStuff(commands.Cog):
                 extra = "Try again?"
                 if dailymode:
                     extra = "Try aga-\noh wait you CAN'T"
+                    try:
+                        if gdict.get(str(interaction.user.id), 0) >=2:
+                            extra = extra + "\n you may now use https://discord.com/channels/773015467753209888/1461631744955514932"
+                            channelrole = ctx.author.guild.get_role(1462230075503149299)
+                            await interaction.user.add_roles(channelrole)
+                            doorlist = {}
+                            try:
+                                try:
+                                    with open("opendoors.json", 'r') as f:
+                                        doorlist = eval(f.read())
+                                    doorlist[str(ctx.author.id)] = 27
+                                    with open("opendoors.json", 'w') as f:
+                                        f.write(str(doorlist))
+                                except FileNotFoundError:
+                                    with open("opendoors.json", "x") as f:
+                                        doorlist[str(ctx.author.id)] = 27
+                                        f.write(str(doorlist))
+                                        await self.printto("made a file since it wasn't there")
+                                except ValueError:
+                                    await self.printto(f"something went HORRIBLY wrong with open doors somehow")
+                            except Exception as e:
+                                await self.printto(f"{e}\n{str(e)}")
+                                return
+                        elif gdict.get(str(interaction.user.id), 0) <= -4:
+                            channelrole = ctx.author.guild.get_role(1462230075503149299)
+                            await ctx.author.remove_roles(channelrole)
+                            extra = extra + f"\n btw you get to steal {round(gdict.get(str(interaction.user.id), 0) * (-2.718281828))} hours from someone with doors time commands :D"
+                            doorlist = {}
+                            try:
+                                try:
+                                    with open("opendoors.json", 'r') as f:
+                                        doorlist = eval(f.read())
+                                    doorlist[str(ctx.author.id)] = round(gdict.get(str(interaction.user.id), 0) * 2.718281828)
+                                    with open("opendoors.json", 'w') as f:
+                                        f.write(str(doorlist))
+                                except FileNotFoundError:
+                                    with open("opendoors.json", "x") as f:
+                                        doorlist[str(ctx.author.id)] = round(gdict.get(str(interaction.user.id), 0) * 2.718281828)
+                                        f.write(str(doorlist))
+                                        await self.printto("made a file since it wasn't there")
+                                except ValueError:
+                                    await self.printto(f"something went HORRIBLY wrong with open doors somehow")
+                            except Exception as e:
+                                await self.printto(f"{e}\n{str(e)}")
+                                return
+                    except Exception as e:
+                        await self.printto(str(e))
+
+
+
                 if ephem:
                     await interaction.response.edit_message(
                         content=f"{failedStr}High Score: {HSDict['High Score']} ({holder})\nPersonal Best: {HSDict.get(str(interaction.user.id), 0)}\nScore: {gdict.get(str(interaction.user.id), 0)}.\n{extra}",
@@ -154,6 +205,7 @@ class DoorStuff(commands.Cog):
         async def corr(interaction: discord.Interaction):
 
             try:
+                other = ""
                 if not public and str(interaction.user.name) != str(ctx.author.name):
                     await self.printto(f"oi {interaction.user.name} piss off")
                     return
@@ -161,11 +213,26 @@ class DoorStuff(commands.Cog):
                     gdict[str(interaction.user.id)] -= 1
                 else:
                     gdict[str(interaction.user.id)] = gdict.get(str(interaction.user.id), 0) + 1
+                    if gdict.get(str(interaction.user.id), 0) >=5:
+                        other = "\n(also +1 https://discord.com/channels/773015467753209888/1461631744955514932 hours for every point past here)"
+                        times = {}
+                        try:
+                            with open("opendoors.json", 'r') as f:
+                                times = eval(f.read())
+                            times[str(interaction.user.id)] = times.get(str(interaction.user.id), 0) + 1
+
+                        except FileNotFoundError:
+                            await self.printto("file didn't exist, making and setting to 1")
+                            times[str(interaction.user.id)] = 1
+                        await self.printto(f"their hours are now at {times.get(str(interaction.user.id), 0)}")
+                        with open("opendoors.json", 'w') as f:
+                            f.write(str(times))
+
                 if random.random() < 1 / 2:
-                    await interaction.response.edit_message(content=f"Score:{gdict.get(str(interaction.user.id), 0)}",
+                    await interaction.response.edit_message(content=f"Score:{gdict.get(str(interaction.user.id), 0)}{other}",
                                                             view=ri)
                 else:
-                    await interaction.response.edit_message(content=f"Score:{gdict.get(str(interaction.user.id), 0)}",
+                    await interaction.response.edit_message(content=f"Score:{gdict.get(str(interaction.user.id), 0)}{other}",
                                                             view=le)
             except Exception as error:
                 await self.printto("An error occurred:", type(error).__name__)
@@ -292,56 +359,62 @@ class DoorStuff(commands.Cog):
     async def daily(self, ctx, ephem: Optional[bool]):
         #checks if day has changed and resets dailist if so
         await ctx.typing()
-        currday = datetime.datetime.today().weekday()
-        serday = -1
-        # TODO: combine all persistent memory into one dict JSON file, because this is getting ridiculous
 
+        pacific = discord.utils.get(ctx.guild.roles, name="Pacific Time -8")
+        central = discord.utils.get(ctx.guild.roles, name="Central Time -6")
+        eastern = discord.utils.get(ctx.guild.roles, name="Eastern Time -5")
+        mountain = discord.utils.get(ctx.guild.roles, name="Mountain Time -7")
+        ceneuro = discord.utils.get(ctx.guild.roles, name="Central European Time +1")
+        t = datetime.datetime.now().hour
+        d = datetime.datetime.now().day
+
+        if pacific in ctx.author.roles:
+            #do nothing
+            await asyncio.sleep(0.001)
+        elif central in ctx.author.roles:
+            if t >= 22:
+                d +=1
+        elif eastern in ctx.author.roles:
+            if t >= 21:
+                d += 1
+        elif mountain in ctx.author.roles:
+            if t == 0:
+                d -= 1
+        elif ceneuro in ctx.author.roles:
+            if t <= 3:
+                d-=1
+
+
+        donelist = {}
         try:
-            with open("weeknum.txt", 'r') as f:
-                line = f.readline()
-                serday = int(line.strip())  # Strip whitespace and convert to int
-        except FileNotFoundError:
-            with open("weeknum.txt", "x") as f:
-                f.write(f"{datetime.datetime.today().weekday()}")
-                serday = 999
-                await self.printto("made a file since it wasn't there, running as if different day")
-        except ValueError:
-            await self.printto(f"something went HORRIBLY wrong with the file somehow")
-        if currday == serday:
-            await self.printto("same day")
-        else:
-            with open("weeknum.txt", "w") as f:
-                f.write(f"{datetime.datetime.today().weekday()}")
-            with open("dailist.json", "w") as f:
-                f.write(str({}))
-
-
-        #checks dailist regardless of day change or not
-        done = {}
-        try:
-            with open("dailist.json", "r") as f:
-                done = eval(f.read())
-        except FileNotFoundError as error:
-            await self.printto(str(error))
-            await self.printto ("file doesn't exist so we makin' it and moving on")
-            with open("dailist.json", "w") as f:
-                f.write(str({}))
-
-
-        if done.get(str(ctx.author.id), False):
-            await ctx.send("Ya did it already -. -", ephemeral=ephem)
-            return
-        else:
             try:
-                with open("dailist.json", "w") as f:
-                    await self.printto("assigning done")
-                    done[str(ctx.author.id)] = True
-                    await self.printto("done")
-                    f.write(str(done))
-            except Exception as error:
-                await self.printto(f"An error occurred:", type(error).__name__)
-                await self.printto(str(error))
-        await self.printto("made it past done check")
+                with open("donely.json", 'r') as f:
+                    donelist = eval(f.read())
+            except FileNotFoundError:
+                with open("donely.json", "x") as f:
+                    donelist[str(ctx.author.id)] = -1
+                    f.write(str(donelist))
+                    await self.printto("made a file since it wasn't there")
+            except ValueError:
+                await self.printto(f"something went HORRIBLY wrong with donely somehow")
+        except Exception as e:
+            await self.printto(f"{e}\n{str(e)}")
+            return
+
+        try:
+            if donelist[str(ctx.author.id)] == d:
+                await ctx.send("ya did it already -. -")
+                return
+            else:
+                donelist[str(ctx.author.id)] = d
+        except KeyError:
+            await self.printto("person not being tracked, adding to file and allowing to play")
+            donelist[str(ctx.author.id)] = d
+        with open("donely.json", 'w') as f:
+            f.write(str(donelist))
+
+
+
 
         if "true" in str(ephem).lower():
             ephem = True
@@ -358,6 +431,7 @@ class DoorStuff(commands.Cog):
             await self.printto("An error occurred:", type(error).__name__)
             await self.printto(str(error))
 
+
         try:
            if random.random() < 1 / 2:
                 await ctx.send("Doors of Daily, think you're lucky?\nhttps://tenor.com/view/oneshot-ballin-gif-8940602852183524649", view=views[1], ephemeral=ephem)
@@ -366,6 +440,128 @@ class DoorStuff(commands.Cog):
         except Exception as error:
             await self.printto("An error occurred:", type(error).__name__)
             await self.printto(str(error))
+
+    @tasks.loop(minutes=60)  # Runs every hour
+    async def timepass(self):
+        await self.printto("updating door hours...")
+        times = {}
+        try:
+            mcb = await self.bot.fetch_guild(773015467753209888)
+            channelrole = mcb.get_role(1462230075503149299)
+
+        except Exception as e:
+            print(str(e))
+        try:
+            with open("opendoors.json", 'r') as f:
+                times = eval(f.read())
+        except FileNotFoundError:
+            await self.printto("file doesn't exist, ignoring...")
+            return
+
+
+        for key in times:
+            try:
+                sucker = await mcb.fetch_member(key)
+
+                await self.printto(f"{sucker.display_name} before: {times[key]}")
+            except Exception as e:
+                await self.printto(str(e))
+
+            if times[key] > 0:
+                times[key] -= 1
+            if times[key] <= 0:
+                try:
+                    await sucker.remove_roles(channelrole)
+                except Exception as e:
+
+                    await self.printto(str(e))
+                if times[key] < 0:
+                    times[key] = int(times[key]/2)
+            await self.printto(f"{sucker.display_name} after: {times[key]}")
+
+        with open("opendoors.json", 'w') as f:
+            f.write(str(times))
+        await self.printto("doors loop successful")
+
+
+    @doors_group.group(name="time", brief="steal | list")
+    async def time_group(self, ctx):
+        await self.printto("obsolete")
+
+    @time_group.command(name="list")
+    async def time_list(self, ctx):
+        doorlist = {}
+        with open('opendoors.json', 'r') as f:
+            try:
+                doorlist = eval(f.read())
+            except Exception as error:
+                await self.printto("An error occurred:", type(error).__name__)
+                await self.printto(str(error))
+        try:
+            tmp = doorlist.copy()
+
+            tmp = dict(sorted(tmp.items(), key=lambda item: item[1], reverse=True))
+            display = {}
+            for s in tmp:
+                try:
+                    display[ctx.guild.get_member(int(s)).display_name] = tmp[s]
+                except Exception as error:
+                    await self.printto(str(error))
+
+            scores = str(display)
+            scores = scores.replace("'", "").replace(",", " hours\n")
+            scores = scores.replace("{", "").replace("}", " hours")
+
+            await ctx.send(scores)
+        except Exception as error:
+            await self.printto("An error occurred:", type(error).__name__)
+
+    @time_group.command(name="steal")
+    async def steal(self, ctx, sucker: discord.Member):
+        doorlist = {}
+        with open('opendoors.json', 'r') as f:
+            try:
+                doorlist = eval(f.read())
+            except Exception as error:
+                await self.printto("An error occurred:", type(error).__name__)
+                await self.printto(str(error))
+                await ctx.send(str(error))
+                return
+        ours = doorlist.get(str(ctx.author.id), 0)
+        if ours >= 0:
+            await ctx.send("you gotta get -4 or beyond in daily to steal from people\n(that lets you steal your score * e hours)\n y'ain't even broke -. -")
+            return
+
+        hours = doorlist.get(str(sucker.id), 0)
+        if hours == 0:
+            await ctx.send("bro they literally have nothing to steal pick someone else\n(maybe run /doors time list)")
+            return
+        elif hours < 0:
+            await ctx.send("bro they're (also) in DEBT\npick someone else.\nIf *only* there was a doors time list command to see who has hours...")
+            return
+
+        result = hours + ours
+        channelrole = ctx.author.guild.get_role(1462230075503149299)
+
+        if result > 0:
+            await ctx.send(f"you steal {abs(ours)} hours from {sucker.display_name}.")
+            doorlist[str(ctx.author.id)] = abs(ours)
+            doorlist[str(sucker.id)] = result
+        elif result < 0:
+            await ctx.send(f"they were kinda broke so you only steal {hours} hour{"s" if hours >  0 else ""} (everything they had).")
+            doorlist[str(ctx.author.id)] = hours
+            doorlist[str(sucker.id)] = 0
+            sucker.remove_roles(channelrole)
+
+        elif result == 0:
+            await ctx.send(f"you steal exactly every single hour they had")
+            doorlist[str(ctx.author.id)] = abs(ours)
+            doorlist[str(sucker.id)] = 0
+            sucker.remove_roles(channelrole)
+
+        await ctx.author.add_roles(channelrole)
+        with open("opendoors.json", 'w') as f:
+            f.write(str(doorlist))
 
 
 async def setup(bot):
